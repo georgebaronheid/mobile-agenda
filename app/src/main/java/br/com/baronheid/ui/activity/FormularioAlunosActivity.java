8ff2bbd2d14cc7fc0584e32aaf1c3a6811605621
George Baronheid <baronheid.george@gmail.com>
2020-03-31 16:01:16 -0300

    TextEdit, AppCompatActivity, setTitle, inputType e Botão

--- app/src/main/java/br/com/baronheid/ui/activity/FormularioAlunosActivity.java
+++ app/src/main/java/br/com/baronheid/ui/activity/FormularioAlunosActivity.java
@@ -12,5 +12,6 @@ public class FormularioAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_alunos);
+        setTitle("Novo Aluno");
     }
 }

e5934f56ea60c5b85002190439404c6369d70faa
George Baronheid <baronheid.george@gmail.com>
2020-03-31 16:51:48 -0300

    Aluno criado, dados buscados do EditText e onClick no botão aplicado

--- app/src/main/java/br/com/baronheid/ui/activity/FormularioAlunosActivity.java
+++ app/src/main/java/br/com/baronheid/ui/activity/FormularioAlunosActivity.java
@@ -1,10 +1,15 @@
 package br.com.baronheid.ui.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import br.com.baronheid.R;
+import br.com.baronheid.model.Aluno;
 
 public class FormularioAlunosActivity extends AppCompatActivity {
 
@@ -13,5 +18,28 @@ public class FormularioAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_alunos);
         setTitle("Novo Aluno");
+
+        final Button botaoSalvar = findViewById(R.id.activity_formulario_alunos_botao_salvar);
+
+        final EditText nomeAluno = findViewById(R.id.activity_formulario_alunos_nome);
+        final EditText telefoneAluno = findViewById(R.id.activity_formulario_alunos_telefone);
+        final EditText emailAluno = findViewById(R.id.activity_formulario_alunos_email);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String nome = nomeAluno.getText().toString();
+                String telefone = telefoneAluno.getText().toString();
+                String email = emailAluno.getText().toString();
+
+                Aluno novoAluno = new Aluno(nome, telefone, email);
+
+                Toast.makeText(FormularioAlunosActivity.this,
+                        "Arroz",
+                        Toast.LENGTH_LONG);
+            }
+        });
+
     }
 }

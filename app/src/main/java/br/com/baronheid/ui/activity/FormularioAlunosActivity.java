3935c9f276cf2aec24af0f01978a396e9a757a31
George Baronheid <baronheid.george@gmail.com>
2020-04-01 15:07:09 -0300

    Refatoração

--- app/src/main/java/br/com/baronheid/ui/activity/FormularioAlunosActivity.java
+++ app/src/main/java/br/com/baronheid/ui/activity/FormularioAlunosActivity.java
@@ -1,6 +1,5 @@
 package br.com.baronheid.ui.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,26 +13,31 @@ import br.com.baronheid.model.Aluno;
 
 public class FormularioAlunosActivity extends AppCompatActivity {
 
+    public static final String APPBAR_NAME = "Novo Aluno";
+    public static final int ACTIVITY_FORMULARIO_ALUNOS = R.layout.activity_formulario_alunos;
+    private Button botaoSalvar;
+    private EditText nomeAluno;
+    private EditText telefoneAluno;
+    private EditText emailAluno;
+
+    //        Foi criado um DAO para transportar o dado persistido de uma view para a outra
+    private final AlunoDao dao = new AlunoDao();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formulario_alunos);
+        setContentView(ACTIVITY_FORMULARIO_ALUNOS);
 
 //        O Set title define o nome da AppBar
-        setTitle("Novo Aluno");
+        setTitle(APPBAR_NAME);
 
-//        Foi criado um DAO para transportar o dado persistido de uma view para a outra
-        final AlunoDao dao = new AlunoDao();
 
         /**
          * Os atributos que estão com final são para permitir acesso a eles a partir de uma
-         * execução da lógica em localmente dentro do ClickListener.
+         * execução da lógica localmente dentro do ClickListener.
          */
-        final Button botaoSalvar = findViewById(R.id.activity_formulario_alunos_botao_salvar);
-
-        final EditText nomeAluno = findViewById(R.id.activity_formulario_alunos_nome);
-        final EditText telefoneAluno = findViewById(R.id.activity_formulario_alunos_telefone);
-        final EditText emailAluno = findViewById(R.id.activity_formulario_alunos_email);
+        instanciaCampos();
 
         /**
          * O método a seguir requer a implementação de uma interface de View.
@@ -41,25 +45,41 @@ public class FormularioAlunosActivity extends AppCompatActivity {
          * O override de onClick acessará os dados de cada EditText, permitindo sua manipulação
          * e criação de objeto de Aluno com os dados pertinentes.
          */
+        configuraFabSalvarAluno();
+
+    }
+
+    private void configuraFabSalvarAluno() {
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                String nome = nomeAluno.getText().toString();
-                String telefone = telefoneAluno.getText().toString();
-                String email = emailAluno.getText().toString();
-
-                Aluno novoAluno = new Aluno(nome, telefone, email);
-
-                dao.salva(novoAluno);
-
-                /**
-                 * Diferentemente do método startActivity, o método abaixo matará a Activity atual,
-                 * trigando o onResume da Activity anterior
-                 */
-                finish();
+                salvaAluno();
             }
         });
+    }
 
+    private void salvaAluno() {
+        String nome = nomeAluno.getText().toString();
+        String telefone = telefoneAluno.getText().toString();
+        String email = emailAluno.getText().toString();
+
+        Aluno novoAluno = new Aluno(nome, telefone, email);
+
+        dao.salva(novoAluno);
+
+        /**
+         * Diferentemente do método startActivity, o método abaixo matará a Activity atual,
+         * trigando o onResume da Activity anterior
+         */
+        finish();
+    }
+
+    private void instanciaCampos() {
+        botaoSalvar = findViewById(R.id.activity_formulario_alunos_botao_salvar);
+
+        nomeAluno = findViewById(R.id.activity_formulario_alunos_nome);
+        telefoneAluno = findViewById(R.id.activity_formulario_alunos_telefone);
+        emailAluno = findViewById(R.id.activity_formulario_alunos_email);
     }
 }
--- app/src/main/java/br/com/baronheid/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/baronheid/ui/activity/ListaAlunosActivity.java
@@ -11,33 +11,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import br.com.baronheid.R;
 import br.com.baronheid.dao.AlunoDao;
 
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
+    public static final String APPBAR_NAME = "Lista de Alunos";
+    public static final int ACTIVITY_LISTA_ALUNOS = R.layout.activity_lista_alunos;
+    private FloatingActionButton botaoAdicionar;
+    private ListView listaDeAlunos;
+    //        O DAO será responsável por carregar os dados com sua lista estática
+    private final AlunoDao alunos = new AlunoDao();
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Lista de Alunos");
+        setTitle(APPBAR_NAME);
 
 //        Referencia a view de conteudo estática utilizando o layout
-        setContentView(R.layout.activity_lista_alunos);
+        setContentView(ACTIVITY_LISTA_ALUNOS);
 
-        FloatingActionButton botaoAdicionar = findViewById(R.id.activity_main_fab_novo_aluno);
-        botaoAdicionar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(
-                        ListaAlunosActivity.this, FormularioAlunosActivity.class
-                ));
-            }
-        });
+        botaoAdicionar = findViewById(R.id.activity_main_fab_novo_aluno);
+        configuraFabOnClick();
 
     }
 
@@ -50,13 +47,25 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        //        O DAO será responsável por carregar os dados com sua lista estática
-        AlunoDao alunos = new AlunoDao();
 
 //        List view permite que a renderização da lista seja dinâmica
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
 
-        /** Não é possivel dar 'add' ou 'set' em um List View. É necessário o uso de um adapter
+        carregaListaAlunos();
+
+    }
+
+    private void configuraFabOnClick() {
+        botaoAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciaActivity();
+            }
+        });
+    }
+
+    private void carregaListaAlunos() {
+        /** Não é possivel dar 'add' ou 'set' em um List View. É necessário o uso de um adapter.
          * O ArrayAdapter é uma implementação facilitada em casos de iteração de listas, e é
          * necessário o uso do R do Android, não o local, buscando um layout que represente um id
          * int.
@@ -65,5 +74,11 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 this,
                 android.R.layout.simple_list_item_1,
                 alunos.todos()));
+    }
+
+    private void iniciaActivity() {
+        startActivity(new Intent(
+                this, FormularioAlunosActivity.class
+        ));
     }
 }
